07f1bb21c3a9353034c308b411189443e8df4e40
ZHIYUAN-PC\Zhi Yuan <zhiyuan_1994@yahoo.com.sg>
2016-09-24 09:17:36 +0800

    add SortCommand class

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,25 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.*;
+
+import java.util.List;
+
+/**
+ * Sorts the address book in alphabetical order and lists it.
+ */
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" 
+            + "Sorts the address book in alphabetical order and lists it.";
+
+    @Override
+    public CommandResult execute() {
+        UniquePersonList allPersons = addressBook.getAllPersonsOriginal();
+        allPersons.sort();
+        List<ReadOnlyPerson> allPersonsSorted = allPersons.immutableListView();
+        return new CommandResult(getMessageForPersonListShownSummary(allPersonsSorted), allPersonsSorted);
+    }
+    
+}
